a81df6ac83a1e2b56b7d0b7d5d79f176c1efaf7e
ReneeShkipper <marinkagnome@gmail.com>
2023-11-13 16:46:17 +0300

    Add project files.

---
+++ Homework_5_1/Program.cs
@@ -0,0 +1,15 @@
+﻿class program
+{ 
+    static float Average(float a, float b)
+    {   
+ 
+        return (a + b) / 2;
+    }
+
+    static void Main(string[] args)
+    {
+        float a = float.Parse(Console.ReadLine());
+        float b = float.Parse(Console.ReadLine());
+        Console.WriteLine( Average(a, b));
+    }
+}
